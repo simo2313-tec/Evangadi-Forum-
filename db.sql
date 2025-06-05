ea50f2885491f2bfab125274d3b8b95de7e54bb6
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-06-05 14:25:21 +0300

    refactor: update column types and constraints in database tables

--- db.sql
+++ db.sql
@@ -1,33 +1,33 @@
 CREATE TABLE IF NOT EXISTS registration (
-  user_id int not null auto_increment,
-  user_name varchar(255) not null,
-  user_email varchar(255) not null,
-  password varchar(100) not null,
+  user_id int NOT NULL AUTO_INCREMENT,
+  user_name varchar(50) NOT NULL,
+  user_email varchar(254) NOT NULL,
+  password varchar(100) NOT NULL,
   PRIMARY KEY (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS profile (
-  user_profile_id int not null auto_increment,
-  user_id int not null,
-  first_name varchar(255) not null,
-  last_name varchar(255) not null,
+  user_profile_id int NOT NULL AUTO_INCREMENT,
+  user_id int NOT NULL,
+  first_name varchar(50) NOT NULL,
+  last_name varchar(50) NOT NULL,
   PRIMARY KEY (user_profile_id),
   FOREIGN KEY (user_id) REFERENCES registration(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS question (
-  question_id int not null auto_increment unique,
-  question_title varchar(100) not null,
+  question_id int NOT NULL AUTO_INCREMENT,
+  question_title varchar(100) NOT NULL,
   question_description text,
   tag varchar(20),
-  user_id int not null,
-  post_id int not null, 
+  user_id int NOT NULL,
+  post_id int NOT NULL UNIQUE,
   PRIMARY KEY (question_id),
   FOREIGN KEY (user_id) REFERENCES registration(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS answer (
-  answer_id int NOT NULL auto_increment,
+  answer_id int NOT NULL AUTO_INCREMENT,
   answer text NOT NULL,
   user_id int NOT NULL,
   question_id int NOT NULL,
@@ -35,4 +35,5 @@ CREATE TABLE IF NOT EXISTS answer (
   FOREIGN KEY (user_id) REFERENCES registration(user_id),
   FOREIGN KEY (question_id) REFERENCES question(question_id)
 );
+
 
