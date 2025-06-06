d7cb59d9d4d8d6839d2efe4dd405c9d80d0cd7c7
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-06-06 12:15:43 +0300

    feat: implement routing for main application and add NotFound page component

--- View/src/App.jsx
+++ View/src/App.jsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Landing from "./Pages/Landing";
+import AskQuestions from "./Pages/AskQuestions";
+import QuestionDetail from "./Pages/QuestionDetialAndAnswer";
+import NotFound from "./Pages/NotFound";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Test</h1>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/ask-questions" element={<AskQuestions />} />
+        <Route path="/question-detail" element={<QuestionDetail />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+
+        {/*<Route path="/profile" element={<Profile />} />
+        <Route path="/reset-password" element={<ResetPassword />} /> */}
+      </Routes>
     </>
   );
 }
