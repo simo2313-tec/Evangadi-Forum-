333d818644e13be4c37104de98780072c204de78
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-06-06 11:21:14 +0300

    feat: add folder structure for  pages Ask Questions, Home, Landing, and Question Detail with corresponding styles module.css files and index.js for entry point.

---
+++ View/src/Pages/AskQuestions/AskQuestions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./askQuestions.module.css";
+
+function AskQuestions() {
+  return (
+    <>
+      <h1 className={styles.title}>Ask Questions Page</h1>
+    </>
+  );
+}
+
+export default AskQuestions;
---
+++ View/src/Pages/QuestionDetialAndAnswer/QuestionDetialAndAnswer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./questionDetialAndAnswer.module.css";
+
+function QuestionDetialAndAnswer() {
+  return (
+    <>
+      <h1 className={styles.title}>Question Detail and Answer Page</h1>
+    </>
+  );
+}
+
+export default QuestionDetialAndAnswer;
