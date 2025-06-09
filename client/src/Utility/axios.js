5d0206644d39e55e0fb85ea78dcb7d54becdd7f4
Mihret7 <mihretbizuayehu777@gmail.com>
2025-06-10 02:30:06 +0300

    code refactored
    code refactored

--- client/src/Utility/axios.js
+++ client/src/Utility/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://7afc93dc-10ea-4928-9f77-c1165022e5cd.mock.pstmn.io",
+  baseURL: "http://localhost:5400/api",
 });
 
 export default api;
