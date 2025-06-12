3db9e6352ae7e2a1e33feb8721353e7efb07d70d
KETEMAASIRES <ketemaasires@gmail.com>
2025-06-11 23:50:02 -0700

    header css modification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,32 @@
 import React from "react";
-// import { Routes, Route } from "react-router-dom";
-// import Home from "./Pages/Home";
-// import Landing from "./Pages/Landing";
-// import AskQuestions from "./Pages/AskQuestions";
-// import QuestionDetail from "./Pages/QuestionDetailAndAnswer";
-// import NotFound from "./Pages/NotFound";
-// import "./App.css";
-// import SignUp from "./Components/SignUp";
-// import Footer from "./Components/Footer/Footer";
-// import Login from "./Components/Login/Login";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Landing from "./Pages/Landing";
+import AskQuestions from "./Pages/AskQuestions";
+import QuestionDetail from "./Pages/QuestionDetailAndAnswer";
+import NotFound from "./Pages/NotFound";
+import "./App.css";
+import SignUp from "./Components/SignUp";
+import Footer from "./Components/Footer/Footer";
+import Login from "./Components/Login/Login";
 import Header from "./Components/Header/Header";
 
 function App() {
   return (
-    <Header/>
-    // <div style={{ height: '100vh', width: '100vw' }}>
-      
-    //   {/* <Routes>
-    //     <Route path="/" element={<Landing />} />
-    //     <Route path="/home" element={<Home />} />
-    //     <Route path="/landing" element={<Landing />} />
-    //     <Route path="/footer" element={<Footer />} />
-    //     <Route path="/ask-questions" element={<AskQuestions />} />
-    //     <Route path="/question-detail" element={<QuestionDetail />} />
-    //     <Route path="/404" element={<NotFound />} />
-    //     <Route path="*" element={<NotFound />} />
-    //     <Route path="/sign-up" element={<SignUp />} />
-    //     <Route path="/login" element={<Login />} />
-    //   </Routes> */}
-    // </div>
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/ask-questions" element={<AskQuestions />} />
+        <Route path="/question-detail" element={<QuestionDetail />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
 
