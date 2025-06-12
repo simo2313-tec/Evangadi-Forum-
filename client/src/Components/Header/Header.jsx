37e0ae9ddd5b519ce136bc4b72153908ae7a229c
Mihret7 <mihretbizuayehu777@gmail.com>
2025-06-12 17:44:13 +0300

    header updated

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -3,20 +3,30 @@ import React, { useState } from "react";
 import styles from "../../../src/Components/Header/style.module.css"; //
 
 import logo from "../../assets/imgs/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../Context/userContext";
 
 const Header = () => {
   const [mobile, setMobile] = useState(false);
+  const [userData, setUserData] = useContext(UserContext);
+  const navigate = useNavigate()
 
   const toggleMobile = () => {
     setMobile((prev) => !prev);
+  };
+
+  const logout = () => {
+    setUserData(null);
+    localStorage.setItem("token", "");
+    navigate("/landing");
   };
 
   return (
     <section className={styles.header_container}>
       <header className={styles.header}>
         <div className={styles.logo}>
-          <img src={logo} alt="ENGADI Logo" />
+          <img src={logo} alt="EVANGADI Logo" />
         </div>
         <div className={styles.navbar}>
           <nav
@@ -27,7 +37,21 @@ const Header = () => {
             <Link to="/home">Home</Link>
             <Link to="#">How it works</Link>
           </nav>
+
+
+          
+          {userData ? (
+        <button onClick={logout} className={styles.sign_in_btn}>
+          LOGOUT
+        </button>
+      ) : (
+        <Link to="/landing">
           <button className={styles.sign_in_btn}>SIGN IN</button>
+        </Link>
+      )}
+
+
+          
           <div className={styles.menu_toggle} onClick={toggleMobile}>
             &#9776;
           </div>
@@ -38,3 +62,7 @@ const Header = () => {
 };
 
 export default Header;
+
+
+
+
