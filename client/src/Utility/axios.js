956b105d3d1ea46d46792f552bbb4d0637ee7f12
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-06-17 04:13:46 +0300

    Update axios configuration to use production API URL and clean up commented code

--- client/src/Utility/axios.js
+++ client/src/Utility/axios.js
@@ -1,34 +1,7 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:5400/api",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   withCredentials: true
-// });
-
-// // Add a request interceptor to add the token to all requests
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default api;
-
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5400/api", // Remove /api from baseURL
+  baseURL: "https://evangadi-forum-t1rh.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
