486fd73a87106d988091cbe55b2717ae8088ab5b
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-06-25 01:50:49 +0300

    fix: Correct rate limit configuration to allow 20 requests per 15 minutes

--- server/middleware/rateLimiter.js
+++ server/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 2, // limit each IP to 20 requests per windowMs
+  max: 20, // limit each IP to 20 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: "Too many requests from this IP, please try again after 15 minutes",
